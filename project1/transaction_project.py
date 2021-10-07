4831a8c753c78bbf71a99782c44f04fac08b8589
William Jessop <willjessop12@gmail.com>
2021-10-07 15:48:07 -0700

    got basic transaction logic working

--- project1/transaction_project.py
+++ project1/transaction_project.py
@@ -13,16 +13,57 @@
 
 # https://github.com/wmacevoy/crypto-wmacevoy
 
-
 import bitcoin
+import bitcoin.rpc
+import bitcoin.wallet
+from bitcoin.wallet import CBitcoinAddress
+from bitcoin.core import COIN, b2lx
+
 bitcoin.SelectParams("testnet")
 
-import bitcoin.rpc
-
 conf_path = "/home/wjessop/.bitcoin/bitcoin.conf"
+proxy = bitcoin.rpc.Proxy(btc_conf_file=conf_path)
 
-proxy = bitcoin.rpc.RawProxy(btc_conf_file=conf_path)
+command = ""
+while True:
+    print("\nWhat would you like to do?\n")
+    print("[send] a transaction")
+    print("[check] balance")
+    print("[quit]\n")
 
-info = proxy.getwalletinfo()
+    command = input()
 
-print("Wallet has a balance of: ", info['balance'])
+    if(command == "send"):
+        print("\nPlease enter the address you would like to send to: ")
+        stringAddr = input()
+
+        print("\nPlease enter the ammount in BTC you would like to send: ")
+        ammount = input()
+
+        print("\nPlease verify the transaction details:")
+        print("Address to send to: ", stringAddr)
+        print("Ammout to send: ", str(ammount))
+        print("\nType [confirm] to send the transaction\n")
+
+        command = input()
+        if(command == "confirm"):
+            print("\nSending transaction...")
+            addr = CBitcoinAddress(stringAddr)
+            txid = proxy.sendtoaddress(addr, float(ammount) * COIN)
+            print("Transaction sent")
+            print("\nTransaction ID: ", b2lx(txid))
+
+        else:
+            print("Returning to main menu")
+    
+    elif(command == "check"):
+        print("\nYour wallet currently has a balance of: ", proxy.getbalance(), " SAT")
+    
+    elif(command == "quit"):
+        break
+
+    else:
+        print("Please check your command and try again")
+
+
+print("\nGoodbye")
