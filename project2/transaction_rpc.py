3ab5a294ad692f94dbd7e7fb05d74201c1c2e648
William Jessop <willjessop12@gmail.com>
2021-12-02 18:42:29 -0800

    finished up rpc transactions

--- project2/transaction_rpc.py
+++ project2/transaction_rpc.py
@@ -1,25 +1,73 @@
 import requests
 from dotenv import load_dotenv
+import os
+import json
 
-env_path = Path('.')/'.env'
+# Example transaction address to find:
+# 2200d4d74606eca205797ea8ee4414af54ee45d5d04b63d29cd5fab8ec133b7c
+
+# Testnet Faucet Address:
+# tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt
+
+# Setting up variables needed for any request & secret variables
+env_path = './.env'
 load_dotenv(dotenv_path=env_path)
 
-# get things from dotenv like this:
-# SECRET_KEY = os.getenv("SECRET_KEY")
+RPC_USER = os.getenv("RPC_USER")
+RPC_PASS = os.getenv("PASSWORD")
+auth = (RPC_USER, RPC_PASS)
 
-def send_transaction(addr, ammount):
-    pass
+url = "http://127.0.0.1:18332"
+
+def get_transaction(addr):
+    body = {
+        "method": "gettransaction",
+        "params": [addr]
+    }
+
+    headers = {
+        "content-type": "application/json"
+    }
+
+    res = requests.post(url=url, json=body, auth=auth, headers=headers)
+
+    print("The following is the transaction you requested: ")
+    print(json.loads(res.content))
+
+def send_transaction(addr, ammt):
+    body = {
+        "method": "sendtoaddress",
+        "params": [addr, float(ammt)]
+    }
+
+    headers = {
+        "content-type": "application/json"
+    }
+
+    res = requests.post(url=url, json=body, auth=auth, headers=headers)
+
+    print("The following is the result of your transaction: ")
+    print(json.loads(res.content))
 
 def check_balance():
-    # balance = somerequest
-    # get the address you want to check
-    print("\nYour wallet currently has a balance of: ", balance, " SAT")
-    pass
+    body = {
+        "method": "getbalance"
+    }
 
+    headers = {
+        "content-type": "application/json"
+    }
+
+    res = requests.post(url=url, json=body, auth=auth, headers=headers)
+
+    print("The following is your wallet balance: ")
+    print(json.loads(res.content))
+    
 command = ""
 while True:
     print("\nWhat would you like to do?\n")
     print("[send] a transaction")
+    print("[get] a transaction")
     print("[check] balance")
     print("[quit]\n")
 
@@ -40,21 +88,34 @@ while True:
         command = input()
         if(command == "confirm"):
             print("\nSending transaction...")
-            send_transaction(stringAddr, ammount)
-            print("Transaction sent")
-            # print("\nTransaction ID: ", b2lx(txid))
+            try:
+                send_transaction(stringAddr, ammount)
+                print("Transaction sent")
+            except:
+                print("There was a problem sending your transaction")
 
         else:
             print("Returning to main menu")
     
     elif(command == "check"):
-        check_balance()
+        try:
+            check_balance()
+        except:
+            print("There was a problem checking your balance")
+
+    elif(command == "get"):
+        print("\nPlease enter the address of the transaction you want to get: ")
+        stringAddr = input()
+
+        try:
+            get_transaction(stringAddr)
+        except:
+            print("There was a problem getting the transaction")
     
     elif(command == "quit"):
         break
 
     else:
         print("Please check your command and try again")
-
 
 print("\nGoodbye")
